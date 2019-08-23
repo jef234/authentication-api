6eccc8e4b4980955a4f116daf877d2fb69624637
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-23 08:17:54 +0530

    heroku config12

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 // const db = require("./helpers/database");
 
-// var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 // const authRouter = require("./routes/auth");
 
@@ -20,13 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 // app.use("/", authRouter)
-app.get('/', function(req, res) {
-  // res.json({message: err.message, error: err});
-  res.json({ title: 'Express' });
-});
+// app.get('/', function(req, res) {
+//   // res.json({message: err.message, error: err});
+//   res.json({ title: 'Express' });
+// });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
