c53cfa76758e911ec4a1e185c416cb1fd4da5971
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-25 15:45:59 +0530

    heroku config20db

--- helpers/database.js
+++ helpers/database.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URI, //process.env.CONNECTION_URL,
+    mongoose.connect(process.env.MONGODB_URI || process.env.CONNECTION_URL,
         { userNewUrlParser: true },
         (err) => {
             if (!err) {
-                console.log("DB Connected!"+process.env.MONGODB_URI+"/heroku_j602q6rk");
+                console.log("DB Connected!"+process.env.MONGODB_URI);
             }
         })
 }
