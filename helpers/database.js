ca5c8988ef052c7301aefb4cbc01e2fbbc69e484
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-25 15:40:20 +0530

    heroku config18db

--- helpers/database.js
+++ helpers/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 exports.connect = () => {
-    mongoose.connect(process.env.CONNECTION_URL|| process.env.MONGODB_URI+"/heroku_j602q6rk",
+    mongoose.connect(process.env.MONGODB_URI+"/heroku_j602q6rk", //process.env.CONNECTION_URL,
         { userNewUrlParser: true },
         (err) => {
             if (!err) {
