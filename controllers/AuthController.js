6183bd3d20d9fb55835debc40889e770623e96e9
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-25 19:26:02 +0530

    debug off

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,6 @@
 const User = require("../models/User")
 
 exports.login = (req, res) => {
-    console.log("body:");
-    console.log(req.body);
-    console.log("email:");
-    console.log(req.body.email);
-    console.log("password:");
-    console.log(req.body.password);
     User.findOne({email:req.body.email})
     .then(user => {
         if(user && user.comparePassword(req.body.password)){
@@ -22,14 +16,6 @@ exports.login = (req, res) => {
 }
 
 exports.register = (req, res) => {
-    console.log("body:");
-    console.log(req.body);
-    console.log("email:");
-    console.log(req.body.email);
-    console.log("name:");
-    console.log(req.body.name);
-    console.log("password:");
-    console.log(req.body.password);
     const user = new User(req.body)
     user.createHash(req.body.password)
     user.save().then(user => {
